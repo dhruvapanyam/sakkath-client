742820ca605c18f833c964644958b50fff5f4d29
Dhruva Panyam <dhruvapanyam16@gmail.com>
2023-08-16 20:35:37 +0530

    ec2 backend

--- src/globals.js
+++ src/globals.js
@@ -1,8 +1,8 @@
 export const server_url = {
     // URL: `http://192.168.0.189`,
     // URL: `http://192.168.0.113`
-    // URL: `https://35.154.194.151`,
-    URL: 'https://sakkath.souravtecken.com'
+    URL: `https://35.154.194.151`,
+    // URL: 'https://sakkath.souravtecken.com'
 }
 
 export function getImage(img){
